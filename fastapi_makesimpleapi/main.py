08d487352ba02577ad9d7c38ce17944398c32f55
Jiyoon Lim <luinpark@gmail.com>
2023-03-12 22:11:30 +0900

    FEATURE: make create app function

--- fastapi_makesimpleapi/main.py
+++ fastapi_makesimpleapi/main.py
@@ -2,5 +2,10 @@ from fastapi import FastAPI
 from fastapi_makesimpleapi.simple_controller import router
 
 
-app = FastAPI()
-app.include_router(router)
+def create_app() -> FastAPI:
+    app = FastAPI()
+    app.include_router(router)
+    return app
+
+
+app = create_app()
